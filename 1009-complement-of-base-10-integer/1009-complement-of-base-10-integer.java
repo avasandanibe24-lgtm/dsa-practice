6c2501d0304f1c675a195a1d585acf94231af3e5
Akash Vasandani <avasandani_be24@thapar.edu>
2026-08-07 10:28:23 +0530

    Time: 0 ms (100%), Space: 42 MB (73.7%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,31 +1,15 @@
 class Solution {
     public int bitwiseComplement(int n) {
-       String s="";
-       int num=0;
-       int rem=0;
-       if(n==0)
-       return 1;
-
-       while(n!=0)
-       {
-        rem=n%2;
-        n=n/2;
-        s=Integer.toString(rem)+s;
-       }
-       int l=s.length();
-       char c;
-       int ans=0;
-       for(int i=0;i<l;i++)
-       {
-        c=s.charAt(i);
-        if(c=='0')
-        {
-           ans=ans+(int)Math.pow(2, l-i-1);
+        int i=0;
+        if (n==0) {
+            return 1;
         }
-        
-       }
-       return ans;
+        int current = n;
+        while (current > 0) {
+            current/=2;
+            i++;
+        }
+        return ((int) Math.pow(2,i)-1-n);
 
-        
     }
 }
