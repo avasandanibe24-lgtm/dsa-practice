7b76caf044706b47d6af457d8fd5eab75be03817
Akash Vasandani <avasandani_be24@thapar.edu>
2026-08-18 18:47:47 +0530

    Time: 0 ms (100%), Space: 42 MB (70.96%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        int ans=dyp(dp,n);
+        return ans;
+        
+    }
+    private int dyp(int[] dp,int n)
+    {
+        if(n<=1)
+        return n;
+        if(dp[n]!=-1)
+        return dp[n];
+        dp[n]=dyp(dp,n-1)+dyp(dp,n-2);
+        return dp[n];
+    }
+
+}
