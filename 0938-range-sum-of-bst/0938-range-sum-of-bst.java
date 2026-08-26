712507de42d5e506e2606d5ea2ef13ee08b7c82a
Akash Vasandani <avasandani_be24@thapar.edu>
2026-08-27 02:35:00 +0530

    Time: 0 ms (100%), Space: 53.8 MB (65.06%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -1,29 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
 
     public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
 
-        return dfs(root, low, high);
-    }
-
-
-    private int dfs(TreeNode root, int low, int high) {
-
-        if(root == null)
-            return 0;
-
-
-        int sum = 0;
-
-
-        if(root.val >= low && root.val <= high) {
-            sum += root.val;
+        if(root.val<low){
+            return rangeSumBST(root.right,low,high);
         }
-
-
-        sum += dfs(root.left, low, high);
-        sum += dfs(root.right, low, high);
-
-
-        return sum;
+        if(root.val>high){
+            return rangeSumBST(root.left,low,high);
+        }
+        return rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high)+root.val;
     }
 }
